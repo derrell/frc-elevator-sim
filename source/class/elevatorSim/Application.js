aa3f5a8888884ab8cb726811bdcf1edb4012746f
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2021-10-28 12:28:51 -0400

    basic premise is working

--- source/class/elevatorSim/Application.js
+++ source/class/elevatorSim/Application.js
@@ -12,6 +12,8 @@
  * @asset(devEnv/*)
  * @ignore Blockly
  */
+var interval;                   // global to allow eval()'ed code to use
+
 qx.Class.define("elevatorSim.Application",
 {
   extend : qx.application.Standalone,
@@ -25,6 +27,7 @@ qx.Class.define("elevatorSim.Application",
       let             doc;
       let             font;
       let             label;
+      let             enabled;
       let             container;
       let             scriptLoader;
       let             elevatorCanvas;
@@ -54,6 +57,28 @@ qx.Class.define("elevatorSim.Application",
       label.setFont(font);
       doc.add(label, { top : 30, left : 10 });
 
+      // Create the Enabled checkbox
+      enabled = new qx.ui.form.ToggleButton("Enabled");
+      enabled.addListener(
+        "changeValue",
+        (e) =>
+        {
+          console.log("Enabled changeValue to", e.getData());
+          this._blockly.getWindow().postMessage(
+            {
+              type  : "control",
+              name  : "enabled",
+              value : e.getData()
+            },
+            "*");
+
+          if (! e.getData())
+          {
+            clearInterval(interval);
+          }
+        });
+      doc.add(enabled, { top : 30, right : 320 });
+
       // Create the container to house the Blockly editor and elevator simulator
       container = new qx.ui.container.Composite(new qx.ui.layout.HBox(10));
       doc.add(container, { edge : 10, top : 60 });
@@ -63,70 +88,37 @@ qx.Class.define("elevatorSim.Application",
         "message",
         (event) =>
         {
+          let             code;
+
           console.log("Application received message:", event);
+
+          switch(event.data.type)
+          {
+          case "control" :
+            switch(event.data.name)
+            {
+            case "run" :
+              code =
+                [
+                  "elevatorSim.Elevator.getInstance().reinit();",
+                  event.data.value,
+                  "interval = setInterval(do_periodically, 100);"
+                ].join("\n");
+              console.log("code to run: " + code);
+              eval(code);
+              break;
+            }
+            break;
+          }
         });
 
       // Add the Blockly editor
       this._blockly = new qx.ui.embed.Iframe("resource/devEnv/blockly.html");
       container.add(this._blockly, { flex : 1 });
 
-      // Send a message to Blockly
-      setTimeout(
-        () =>
-        {
-          this._blockly.getWindow().postMessage("back at you!", "*");
-        },
-        1000);
-
       // Add the elevator simulator
-      elevatorCanvas = new elevatorSim.Elevator();
+      elevatorCanvas = elevatorSim.Elevator.getInstance();
       container.add(elevatorCanvas);
-    }
-  },
-
-  statics :
-  {
-    TOOLBOX :
-    {
-      "kind": "categoryToolbox",
-      "contents": [
-        {
-          "kind": "category",
-          "name": "Control",
-          "contents": [
-            {
-              "kind": "block",
-              "type": "controls_if"
-            },
-            {
-              "kind": "block",
-              "type": "controls_whileUntil"
-            },
-            {
-              "kind": "block",
-              "type": "controls_for"
-            }
-          ]
-        },
-        {
-          "kind": "category",
-          "name": "Logic",
-          "contents": [
-            {
-              "kind": "block",
-              "type": "logic_compare"
-            },
-            {
-              "kind": "block",
-              "type": "logic_operation"
-            },
-            {
-              "kind": "block",
-              "type": "logic_boolean"
-            }
-          ]
-        }
-      ]
     }
   }
 });
--- source/class/elevatorSim/Elevator.js
+++ source/class/elevatorSim/Elevator.js
@@ -8,28 +8,30 @@
 
 qx.Class.define("elevatorSim.Elevator",
 {
+  type   : "singleton",
   extend : qx.ui.container.Composite,
 
   construct : function()
   {
+    let             grid;
     let             buttons;
     let             elevator;
     let             doorLeft;
     let             doorRight;
     let             opening;
+    let             butUp;
+    let             butDown;
     let             butFloor1;
     let             butFloor2;
     let             butFloor3;
-    let             butDoorOpen;
-    let             butDoorClose;
+    let             status;
 
     this.base(arguments);
 
     // Set overall elevator attributes
     this.set(
       {
-        width           : 400,
-        maxHeight       : 420,
+        width           : 300,
         layout          : new qx.ui.layout.Canvas(),
         backgroundColor : "white"
       });
@@ -39,38 +41,113 @@ qx.Class.define("elevatorSim.Elevator",
     // Numbers are top pixel of each floor
     //
     [ 10, 135, 260, 385 ].forEach(
-      (top, i) =>
+      (bottom, i) =>
       {
         let             floor = new qx.ui.core.Widget();
 
         floor.set(
           {
             height          : 10,
-            width           : 280,
+            width           : 180,
             backgroundColor : "black"
           });
 
         this.add(
           floor,
           {
-            left : 60,
-            top  : top
+            left   : 60,
+            bottom : bottom
           });
 
         // Add the floor label
-        if (i > 0)
+        if (i < 3)
         {
-          let label = new qx.ui.basic.Label("" + (4 - i));
+          let label = new qx.ui.basic.Label("" + (i + 1));
           label.setFont("bold");
           this.add(
             label,
             {
-              left : 20,
-              top  : top - 70
+              left   : 20,
+              bottom : bottom + 60
             });
         }
       });
 
+    // Add the button container
+    buttons = new qx.ui.container.Composite(new qx.ui.layout.Grid(10, 4));
+    this.add(
+      buttons,
+      {
+        left   : 20,
+        top    : 0
+      });
+
+    //
+    // Add the buttons
+    //
+
+    butFloor1 = new qx.ui.form.Button("1");
+    butFloor1.addListener("pointerdown",
+                          (e) => { this._state.button["1"] = true; });
+    butFloor1.addListener("pointerup",
+                          (e) => { this._state.button["1"] = false; });
+    butFloor1.addListener("pointerout",
+                          (e) => { this._state.button["1"] = false; });
+    buttons.add(butFloor1, { row : 2, column : 1 });
+
+    butFloor2 = new qx.ui.form.Button("2");
+    butFloor2.addListener("pointerdown",
+                          (e) => { this._state.button["2"] = true; });
+    butFloor2.addListener("pointerup",
+                          (e) => { this._state.button["2"] = false; });
+    butFloor2.addListener("pointerout",
+                          (e) => { this._state.button["2"] = false; });
+    buttons.add(butFloor2, { row : 1, column : 1 });
+
+    butFloor3 = new qx.ui.form.Button("3");
+    butFloor3.addListener("pointerdown",
+                          (e) => { this._state.button["3"] = true; });
+    butFloor3.addListener("pointerup",
+                          (e) => { this._state.button["3"] = false; });
+    butFloor3.addListener("pointerout",
+                          (e) => { this._state.button["3"] = false; });
+    buttons.add(butFloor3, { row : 0, column : 1 });
+
+    butUp = new qx.ui.form.Button("Up");
+    butUp.addListener("pointerdown",
+                          (e) => { this._state.button["Up"] = true; });
+    butUp.addListener("pointerup",
+                          (e) => { this._state.button["Up"] = false; });
+    butUp.addListener("pointerout",
+                          (e) => { this._state.button["Up"] = false; });
+    buttons.add(butUp, { row : 0, column : 0 });
+
+    butDown = new qx.ui.form.Button("Down");
+    butDown.addListener("pointerdown",
+                          (e) => { this._state.button["Down"] = true; });
+    butDown.addListener("pointerup",
+                          (e) => { this._state.button["Down"] = false; });
+    butDown.addListener("pointerout",
+                          (e) => { this._state.button["Down"] = false; });
+    buttons.add(butDown, { row : 2, column : 0 });
+
+    //
+    // Add the status fields
+    //
+    grid = new qx.ui.layout.Grid(10, 4);
+    status = new qx.ui.container.Composite(grid);
+    this.add(
+      status,
+      {
+        left   : 160,
+        top    : 0
+      });
+
+    status.add(new qx.ui.basic.Label("Encoder:"), { row : 0, column : 0 });
+
+    // Create the label where we'll place the "encoder" value
+    this._statusElevatorEncoder = new qx.ui.basic.Label("20");
+    status.add(this._statusElevatorEncoder, { row : 0, column : 1 });
 
     //
     // Add the elevator
@@ -80,17 +157,12 @@ qx.Class.define("elevatorSim.Elevator",
     elevator = new qx.ui.container.Composite(new qx.ui.layout.HBox());
     elevator.set(
       {
-        height          : 100,
-        width           : 200,
+        height          : 115,
+        width           : 100,
         padding         : 10,
         backgroundColor : "#dddddd"
       });
     this._elevator = elevator;
-
-    // Add the button container
-    buttons = new qx.ui.container.Composite(new qx.ui.layout.Grid(10,4));
-    buttons.setMarginRight(10);
-    elevator.add(buttons);
 
     // Add the left door
     doorLeft = new qx.ui.basic.Atom();
@@ -126,88 +198,105 @@ qx.Class.define("elevatorSim.Elevator",
     this.add(
       elevator,
       {
-        left : 100,
-        top  : 270
+        left   : 100,
+        bottom : 20
       });
-
-    //
-    // Add the buttons
-    //
-
-    butFloor1 = new qx.ui.form.Button("1");
-    butFloor1.addListener(
-      "execute",
-      (e) =>
-      {
-        this.move(80);
-      });
-    buttons.add(butFloor1, { row : 2, column : 0 });
-
-    butFloor2 = new qx.ui.form.Button("2");
-    butFloor2.addListener(
-      "execute",
-      (e) =>
-      {
-        this.move(0);
-      });
-    buttons.add(butFloor2, { row : 1, column : 0 });
-
-    butFloor3 = new qx.ui.form.Button("3");
-    butFloor3.addListener(
-      "execute",
-      (e) =>
-      {
-        this.move(-80);
-      });
-    buttons.add(butFloor3, { row : 0, column : 0 });
-
-    butDoorOpen = new qx.ui.form.Button("Open");
-    butDoorOpen.addListener(
-      "execute",
-      (e) =>
-      {
-      });
-    buttons.add(butDoorOpen, { row : 0, column : 1 });
-
-    butDoorClose = new qx.ui.form.Button("Close");
-    butDoorClose.addListener(
-      "execute",
-      (e) =>
-      {
-      });
-    buttons.add(butDoorClose, { row : 1, column : 1 });
   },
 
   members :
   {
-    _timer    : null,
-    _state    : "doors-closed",
-    _elevator : null,
+    _timer                 : null,
+    _elevator              : null,
+    _statusElevatorEncoder : null,
+    _state                 :
+    {
+      encoder                : 20,
+      velocity               : 0,
+      button                 :
+      {
+        Up                     : false,
+        Down                   : false,
+        "1"                    : false,
+        "2"                    : false,
+        "3"                    : false
+      }
+    },
 
     /**
-     * Move the elevator up or down
-     *
-     * @param speed {Number}
-     *   Speed, in the range [ -100, 100 ].
-     *   Positive values cause the elevator to go down; negative, up.
-     *   Speed 0 means stop.
+     * Reinitialize the elevator to its initial location
      */
-    move : function(speed)
+    reinit : function()
     {
-      let             distance = speed > 0 ? 1 : -1;
+      // Reset the encoder to the initial position
+      this._state.encoder = 20;
 
-      // Limit the speed to range [ -100, 100 ] and we need only its
+      // Move the elevator to the initial position
+      this._elevator.setLayoutProperties(
+        {
+          bottom : this._state.encoder
+        });
+
+      // Reset the background color to indicate no crash
+      this.setBackgroundColor("white");
+    },
+
+    /**
+     * Return the specified button's state
+     *
+     * @param buttonId {String}
+     *   The ID (label) of the button whose value is requested
+     *
+     * @return {Boolean}
+     *   Whether the specified button is current pressed
+     */
+    isButtonPressed : function(buttonId)
+    {
+      return this._state.button[buttonId];
+    },
+
+    /**
+     * Get the current encoder value
+     */
+    getEncoder : function()
+    {
+      return this._state.encoder;
+    },
+
+    /**
+     * Get the most recently set velocity
+     */
+    getVelocity : function()
+    {
+      return this._state.velocity;
+    },
+
+    /**
+     * Move the elevator up or down at a given velocity
+     *
+     * @param velocity {Number}
+     *   Velocity, in the range [ -100, 100 ].
+     *   Positive values cause the elevator to go down; negative, up.
+     *   Velocity 0 means stop.
+     */
+    setVelocity : function(velocity)
+    {
+      let             distance = velocity > 0 ? -1 : 1;
+
+      // Limit the velocity to range [ -100, 100 ] and we need only its
       // magnitude, as we've already pulled the sign off to use as the
       // distance to move on each timer expiration
-      speed = Math.abs(speed);
-      if (speed > 100)
+      velocity = Math.abs(velocity);
+      if (velocity > 100)
       {
-        speed = 100;
+        velocity = 100;
       }
-      if (speed < 0)
+      if (velocity < 0)
       {
-        speed = 0;
+        velocity = 0;
       }
+
+      // Save the new velocity in the elevator state
+      this._state.velocity = velocity;
 
       // Stop any existing timer
       if ( this._timer)
@@ -217,62 +306,71 @@ qx.Class.define("elevatorSim.Elevator",
       }
 
       // If we were asked to stop moving...
-      if (speed === 0)
+      if (velocity === 0)
       {
         // ... then there's nothing more to do
         return;
       }
 
       // Create a timer for the animation. The interval is the inverse
-      // of speed: a higher speed means that the interval is smaller.
-      this._timer = new qx.event.Timer(100 - speed);
+      // of velocity: a higher velocity means that the interval is smaller.
+      this._timer = new qx.event.Timer(100 - velocity);
 
       // Handle each frame of the animation
       this._timer.addListener(
         "interval",
         function()
         {
-          var   newTop;
+          let             newBottom;
+          let             bOutOfBounds = false;
             
-          // Get the new top position of the elevator by adding the
+          // Get the new bottom position of the elevator by adding the
           // positive or negative distance
-          newTop = this._elevator.getLayoutProperties().top + distance;
+          newBottom = this._elevator.getLayoutProperties().bottom + distance;
           
           // Don't get any lower than the minimum
-          if (newTop > 300)
+          if (newBottom > 300)
           {
-            newTop = 300;
+            newBottom = 300;
           }
 
           // Don't get any higher than the maximum
-          if (newTop < 5)
+          if (newBottom < 0)
           {
-            newTop = 5;
+            newBottom = 0;
           }
+
+          if (newBottom > 290 || newBottom < 15)
+          {
+            bOutOfBounds = true;
+          }
+
+          // Set the background color to indicate out of bounds, if necessary
+          this.setBackgroundColor(bOutOfBounds ? "red" : "white");
+
+          // Update the "encoder" elevator state and readout
+          this._statusElevatorEncoder.setValue(newBottom.toString());
+          this._state.encoder = newBottom;
 
           // Cause the elevator to move in the canvas
           this._elevator.setLayoutProperties(
               {
-                top : newTop
+                bottom : newBottom
               });
           
           // Have we reached our hard-stop ending point?
-          if ((distance > 0 && newTop >= 300) ||
-              (distance < 0 && newTop <= 5))
+          if ((distance > 0 && newBottom >= 300) ||
+              (distance < 0 && newBottom <= 0))
           {
             // Yup. Animation is complete
             this._timer.dispose();
             this._timer = null;
-
-            // Change state
-            this._state = "doors-closed";
           }
         },
         this);
       
       // Begin the animation
       this._timer.start();
-      
     }
   }
 });
